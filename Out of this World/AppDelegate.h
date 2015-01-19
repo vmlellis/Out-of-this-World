8a32179a50fc8c68dd7c687dfa4336b5d251fdd7
Victor Lellis <lellis@Victors-MacBook-Pro.local>
2015-01-19 05:29:46 -0800

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Victor Lellis on 19/01/15.
+//  Copyright (c) 2015 Victor Lellis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this World/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Out of this World
+//
+//  Created by Victor Lellis on 19/01/15.
+//  Copyright (c) 2015 Victor Lellis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
